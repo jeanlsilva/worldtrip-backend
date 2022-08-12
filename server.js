bd4caf33479aee3eab4fab8f51127f9283c59eed
jean <jeanslucasilva@gmail.comm>
2022-08-11 20:01:45 -0400

    server port config

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const router = jsonServer.router('server.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3333; // <== You can change the port
 
+console.log(port)
+
 server.use(middlewares);
 server.use(router);
 
